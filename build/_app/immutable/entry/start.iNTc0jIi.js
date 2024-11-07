44a8b4ef68ea38480a435e9b697108f2fbbb1a54
sai <0148@htl.rennweg.at>
2024-11-07 18:28:23 +0100

    ready for apache

---
+++ build/_app/immutable/entry/start.iNTc0jIi.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DyquKPJQ.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -3,22 +3,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			fallback: 'index.html' // Specify your fallback HTML file
-		  }),
-		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/Svelte1' : '',
-        }
-		
-	}
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html',
+    }),
+    paths: {
+      base: '' // Set this to an empty string for root hosting
+    }
+  }
 };
 
 export default config;
